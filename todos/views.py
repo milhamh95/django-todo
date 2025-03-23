1c267f1822cf062f58a287233965b910a0a1ec5b
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-03-23 19:57:13 +0700

    feat: load todo list

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Todo
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, "index.html")
+        todos = Todo.objects.all()
+        context = {
+            "todos": todos,
+        }
+        return render(request, "index.html", context)
