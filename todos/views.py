09e066e303db484adaa34dcb4781bc135caddb40
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-03-23 20:27:08 +0700

    feat: create todo

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Todo
 
@@ -10,3 +10,11 @@ class IndexView(View):
             "todos": todos,
         }
         return render(request, "index.html", context)
+    
+    def post(self, request):
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+
+        Todo.objects.create(title=title, content=content, user=request.user)
+
+        return redirect("index_view")
