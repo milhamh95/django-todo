18fec7bd58af5383e54c8366517eef4dd5cc6fce
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-04-03 12:38:20 +0700

    ref: remove all

--- todos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Todo
-
-# Register your models here.
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ("title", "content", "is_done", "created_at")
--- todos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import IndexView, DetailView
-
-urlpatterns = [
-    path("", IndexView.as_view(), name="index_view"),
-    path("todos/<str:id>/", DetailView.as_view(), name="detail_view")
-]
