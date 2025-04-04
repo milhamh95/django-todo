a3469d02d35bc999c7c1bcb98fc001fdfb0c5d74
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-04-04 09:26:09 +0700

    feat: add todo list view route

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+class TodoListView(ListView):
+    template_name = 'list.html'
+    context_object_name = 'todos'
+
+    def get_queryset(self):
+        return [
+            {'id': 1, 'title': 'Learn Django', 'completed': False},
+            {'id': 2, 'title': 'Build Todo App', 'completed': True},
+            {'id': 3, 'title': 'Write Tests', 'completed': False},
+        ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ini List Todo'
+        return context
