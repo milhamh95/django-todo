076a7de19dcf7e4cc7fbb87f2fd19db2450855fc
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2025-04-04 19:51:56 +0700

    feat: register feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 class LoginView(View):
@@ -32,4 +33,39 @@ class RegisterView(View):
         return render(request, "register.html")
 
     def post(self, request):
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password != password2:
+            print("Passwords do not match")
+            return render(
+                request,
+                'register.html',
+                {"error": "Passwords do not match"})
+
+        if User.objects.filter(username=username).exists():
+            return render(
+                request,
+                'register.html',
+                {"error": "Username already exists"})
+
+        if User.objects.filter(email=email).exists():
+            return render(
+                request,
+                'register.html',
+                {"error": "Email already exists"})
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+
+        login(request, user)
         return redirect('todo_list')
